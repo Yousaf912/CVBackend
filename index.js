8d537d56f31b09fe22ed071452cb20599c6eb24d
Muhammad Yousaf <yousafva9@gmail.com>
2024-12-26 22:09:13 +0500

    updated

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 require("dotenv").config()
 const express = require("express");
 const mongoose = require("mongoose");
-const  router = require("./Routes/Routes");
-const cors =require("cors")
+const router = require("./Routes/Routes");
+const cors = require("cors")
 const App = express();
 const port = process.env.PORT;
-const mongourl =process.env.MONGOURL;
+const mongourl = process.env.MONGOURL;
 
 
 let corsOptions = {
-    origin:'http://localhost:5173',
-    methods:'GET,POST,PUT,DELETE',
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: [
-        "Content-Type",            
-        "Authorization",           
-        "X-Requested-With",       
-        "Accept",                
-        "Access-Control-Allow-Origin", 
-        "application/javascript",  
-      ],         
-      preflightContinue: true,  
-    credentials:true,
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "Accept",
+        "Access-Control-Allow-Origin",
+        "application/javascript",
+    ],
+    preflightContinue: true,
+    credentials: true,
 }
 App.use(cors(corsOptions))
 App.use(express.json());
-App.use('/',router)
+App.use('/', router)
 
 
-App.listen(port,()=>{
-    console.log(`server is running on ${port}`);
-    mongoose.connect(mongourl).then(()=>{
-        console.log('mongoDb is connacted');
-    }).catch((er)=>{
-        console.log(er);
-        
+
+
+mongoose.connect(mongourl).then(() => {
+    console.log('mongoDb is connacted');
+    App.listen(port, () => {
+        console.log(`server is running on ${port}`)
+
     })
+}).catch((er) => {
+    console.log(er);
+
 })
